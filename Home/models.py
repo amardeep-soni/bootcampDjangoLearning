a8096501d347aa36bb9ed845492badf76e14dca3
amardeep-soni <amardeepmunni@gmail.com>
2024-10-18 05:47:33 +0545

    created model for saving personal info and also register in admin to create it admin page also in setting and urls page configure to save uploaded files in media and for displaying

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db import models
 
 # Create your models here.
+class Home(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    resume_file = models.FileField(upload_to="results/")
+    image = models.ImageField(upload_to="pictures/")
+    about_me = models.TextField()
+
+    def __str__(self):
+        return self.name
