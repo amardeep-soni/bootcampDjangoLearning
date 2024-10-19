4e1666426b45ae197f47591e930fc834be971415
amardeep-soni <amardeepmunni@gmail.com>
2024-10-19 22:30:55 +0545

    created testimonial model and render in html to reflect

--- Home/models.py
+++ Home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db import models
 
+
 # Create your models here.
 class Home(models.Model):
     name = models.CharField(max_length=100)
@@ -9,8 +10,15 @@ class Home(models.Model):
     image = models.ImageField(upload_to="pictures/")
     about_me = models.TextField()
 
+
 class Service(models.Model):
     service_icon = models.CharField(max_length=100)
     service_title = models.CharField(max_length=100)
     service_desc = models.TextField()
 
+
+class Testimonial(models.Model):
+    image = models.ImageField(upload_to="pictures/")
+    name = models.CharField(max_length=100)
+    message = models.TextField()
+    compnay_position = models.CharField(max_length=100)
--- Home/views.py
+++ Home/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Home, Service
+from .models import Home, Service, Testimonial
 
 
 # Create your views here.
 def index(request):
     data = Home.objects.all()
     service = Service.objects.all()
-    return render(request, "index.html", {"data": data, "services": service})
+    testimonial = Testimonial.objects.all()
+    context = {"data": data, "services": service, "testimonials": testimonial}
+
+    return render(request, "index.html", context)
